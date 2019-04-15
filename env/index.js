f3cba1ec54b281aed92ca79642b209cdf00777ed
shianqi <mail@shianqi.com>
2019-04-15 23:55:20 +0800

    Use the full loadsh

--- env/index.js
+++ env/index.js
@@ -1,6 +1,6 @@
-const merge = require('lodash.merge')
+const _ = require('lodash')
 const common = require('./env/base')
 const env = process.env.BUILD_ENV || 'development'
 const config = require(`./env/${env}`)
 
-module.exports = merge(common, config, { env })
+module.exports = _.merge(common, config, { env })
